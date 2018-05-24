f142a33575a1123c3c61c8768d047111eebf509e
Maxwell Plotkin <plotkinmaxwell@gmail.com>
2018-05-24 14:54:21 -0700

    rng game from trpl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret = rand::thread_rng().gen_range(1,10);
+
+    println!("Pick a number: ");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Input Failed");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number.");
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small."),
+        Ordering::Greater => println!("Too large."),
+        Ordering::Equal => println!("Correct."),
+    }
 }
